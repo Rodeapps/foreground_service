9e23eb4a036179737a1934c752a9d76188233397
Anca Mihesan <anca.mihesan@rodeapps.com>
2020-12-23 14:52:22 +0200

    testing

--- android/src/main/kotlin/org/thebus/foreground_service/TestForegroundService.kt
+++ android/src/main/kotlin/org/thebus/foreground_service/TestForegroundService.kt
@@ -1,7 +1,13 @@
 package org.thebus.foreground_service
 
 import android.app.IntentService
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 
 /**
  * Created by ancamihesan on 12/23/20. Email anca.mihesan@rodeapps.com
@@ -9,7 +15,44 @@ import android.content.Intent
  */
 class TestForegroundService : IntentService("org.thebus.foreground_service.TestForegroundService") {
     override fun onHandleIntent(intent: Intent?) {
-        val notificationHelper = ForegroundServicePlugin.NotificationHelper();
-        startForeground(notificationHelper.notificationId, notificationHelper.currentNotification)
+        startForeground(1, createNotification())
     }
+
+
+    private fun createNotification(): Notification {
+        val notificationChannelId = "ENDLESS SERVICE CHANNEL"
+
+        // depending on the Android API that we're dealing with we will have
+        // to use a specific method to create the notification
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager;
+            val channel = NotificationChannel(
+                    notificationChannelId,
+                    "Endless Service notifications channel",
+                    NotificationManager.IMPORTANCE_HIGH
+            ).let {
+                it.description = "Endless Service channel"
+                it.enableLights(true)
+                it.lightColor = Color.RED
+                it.enableVibration(true)
+                it.vibrationPattern = longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 400)
+                it
+            }
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val builder: Notification.Builder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) Notification.Builder(
+                this,
+                notificationChannelId
+        ) else Notification.Builder(this)
+
+        return builder
+                .setContentTitle("Endless Service")
+                .setContentText("This is your favorite endless service working")
+//                .setSmallIcon(R.mipmap.ic_launcher)
+                .setTicker("Ticker text")
+                .setPriority(Notification.PRIORITY_HIGH) // for under android 26 compatibility
+                .build()
+    }
+}
 }
